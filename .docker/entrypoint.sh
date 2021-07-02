7c9bdd388c8330442b0995d2a461b04764d10805
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-03 00:21:52 +0530

    Updates extensive configuration of uvicorn (#5)
    * Updates extensive configuration of uvicorn
    
    * Updates run condition for pipeline
    
    * Updates command for dev env
    
    * Updates optional dependencies for uvicorn
    
    * Provides log config for uvicorn

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
 
-uvicorn --host 0.0.0.0 --port 8000 src.app:create_app
+uvicorn \
+    --factory \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --workers 2 \
+    --log-config config/log_config.json \
+    --access-log \
+    --loop uvloop \
+    --http httptools \
+    --ws none \
+    src.app:create_app
