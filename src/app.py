19a60df7262d690e5516229906cc59a954e94482
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-03 02:38:03 +0530

    Adds health router (#6)
    * Adds router subpackage
    
    * Adds health module in router subpackage
    
    * Registers health router in app
    
    * Adds pyenv version file
    
    * Fixes isort issue
    
    * Updates pyenv file in dockerignore

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
 from fastapi import FastAPI
 
+from .routers.health import router as health_router
+
 
 def create_app():
     app = FastAPI()
 
-    @app.get("/health")
-    def health():
-        return {"status": "healthy"}
+    app.include_router(health_router, prefix="/health")
 
     return app
