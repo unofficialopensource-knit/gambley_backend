0197fc9d47b5b8f7270dc71bda47897c3defad9c
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2021-07-05 14:54:44 +0530

    Adds tags and metadata for doc (#9)

--- src/app.py
+++ src/app.py
@@ -4,8 +4,15 @@ from .routers.health import router as health_router
 
 
 def create_app():
-    app = FastAPI()
+    app = FastAPI(
+        title="Gambley Backend",
+        description="Backend for the gambley project",
+        redoc_url=None,
+        openapi_tags=[
+            {"name": "health", "description": "Operations related to health check"}
+        ],
+    )
 
-    app.include_router(health_router, prefix="/health")
+    app.include_router(health_router, prefix="/health", tags=["health"])
 
     return app
