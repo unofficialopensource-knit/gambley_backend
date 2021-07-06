92194a1deea831149a5e647d7aad6f9c5231147a
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-07 00:24:32 +0530

    test commit

--- scripts/setup_test_data.sh
+++ scripts/setup_test_data.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Created test data directory"
 mkdir test-data
 cd test-data
+
+echo "Copying test data to test data directory"
 aws s3 cp s3://gambley-test-data/backend/email.json .
