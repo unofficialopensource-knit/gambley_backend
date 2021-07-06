1250230cc3ccecddb103f92d0ba9b3e9017f7c6c
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-07 02:14:29 +0530

    Adds test for sending mail

--- test/test_email.py
+++ test/test_email.py
@@ -1,5 +1,6 @@
 from pytest import mark
 
+from src.dependencies import fast_mail
 from .conftest import load_test_data
 
 
@@ -16,3 +17,21 @@ def test_send_email_fails_request_validation(test_client, test_data):
 
 
 # Test email is sent 202 status code
+@mark.parametrize("test_data", load_test_data("email", "success"))
+def test_send_email_passes(test_client, test_data):
+    fast_mail.config.SUPPRESS_SEND = 1
+
+    with fast_mail.record_messages() as outbox:
+        response = test_client.post("/api/email/new", json=test_data["payload"])
+
+        assert response.status_code == 202
+
+        response_data = response.json()
+        assert "receiver" in response_data.keys()
+        assert isinstance(response_data["receiver"], list)
+        assert len(response_data["receiver"]) == len(test_data["payload"]["receiver"])
+
+        assert len(outbox) == len(test_data["payload"]["receiver"])
+
+        for index, item in enumerate(outbox):
+            assert item["To"] == test_data["payload"]["receiver"][index]
