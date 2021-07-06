83c0203a28f1958e892c93eee1f5695f5c8a9ce8
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-06 23:47:23 +0530

    Updates test case for validation error

--- test/test_email.py
+++ test/test_email.py
@@ -1,14 +1,17 @@
-# Email send works fine
-def test_send_email_pass(test_client):
-    response = test_client.post(
-        "/api/email/new",
-        json={
-            "sender": "string",
-            "receiver": "string",
-            "subject": "string",
-            "template_id": 0,
-            "template_params": {},
-        },
-    )
+from pytest import mark
 
-    assert response.status_code == 201
+from .conftest import load_test_data
+
+
+# Test for all cases of validation errors 422
+@mark.parametrize("test_data", load_test_data("email", "invalid_request"))
+def test_send_email_fails_request_validation(test_client, test_data):
+    response = test_client.post("/api/email/new", json=test_data["payload"])
+
+    assert response.status_code == 422
+
+    response_data = response.json()
+    assert isinstance(response_data["detail"], list)
+    assert test_data["invalid_attribute"] in response_data["detail"][0]["loc"]
+
+# Test email is sent 202 status code
