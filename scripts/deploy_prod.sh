f31774d97b2a27abdd8032cc7be2a04b484d4f5a
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-10 18:10:49 +0530

    Adds script for deployment

---
+++ scripts/deploy_prod.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+hostname -i
+
+while IFS='=' read -r -d '' SECRET VALUE;
+do
+    if [[ $SECRET == "GAMBLEY_*" ]];
+    then
+        SECRET_NAME=${SECRET/#GAMBLEY_}
+        echo $SECRET_NAME
+        echo $VALUE | sudo docker secret create $SECRET_NAME
+    fi
+done < <(env -0)
+
+sed --in-place "s/BACKEND_IMAGE_TAG/$IMAGE_TAG/g" .docker/stack.yml
+
+sudo docker stack deploy --compose-file .docker/stack.yml --orchestrator swarm
