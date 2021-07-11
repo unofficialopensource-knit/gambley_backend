757f21b004db0c5a2dcd12dbd79530f027f4e63f
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-12 00:54:19 +0530

    Updates cut command

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -6,7 +6,7 @@ aws s3 cp s3://gambley-app-data/configs/gambley.env .
 aws s3 cp s3://gambley-app-data/stack/gambley-stack.yml .
 
 aws ecr get-login-password --region ap-south-1 | sudo docker login --username AWS --password-stdin 783729352349.dkr.ecr.ap-south-1.amazonaws.com
-export TAG=`grep image gambley-stack.yml | cut -d': ' -f3`
+export TAG=`grep image gambley-stack.yml | cut -d':' -f3`
 
 echo $TAG
 sudo docker image pull 783729352349.dkr.ecr.ap-south-1.amazonaws.com:"$TAG"
