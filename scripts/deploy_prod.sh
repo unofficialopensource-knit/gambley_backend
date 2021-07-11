7d203dff1ea5b71c5f60fc564dc261320e12af48
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-12 00:03:34 +0530

    Deploys docker stack

--- scripts/deploy_prod.sh
+++ scripts/deploy_prod.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# sudo docker stack deploy --compose-file .docker/stack.yml --orchestrator swarm gambley
 cd /tmp
 
 aws s3 cp s3://gambley-app-data/configs/gambley.env .
 aws s3 cp s3://gambley-app-data/stack/gambley-stack.yml .
 
-ls -lha
+sudo docker stack deploy --compose-file .docker/stack.yml --orchestrator swarm gambley
