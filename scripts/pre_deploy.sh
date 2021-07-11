8f282f973a43199ef70a22badfd447fd0d771d5b
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-12 02:18:38 +0530

    Fixes SH-2129

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -4,9 +4,11 @@ aws s3 cp s3://gambley-infra-data/private_keys/id_gambley .
 chmod 400 id_gambley
 sed --in-place "s/BACKEND_IMAGE_TAG/$IMAGE_TAG/g" .docker/stack.yml
 
-echo "GAMBLEY_MAIL_USERNAME=$GAMBLEY_MAIL_USERNAME" >> gambley.env
-echo "GAMBLEY_MAIL_PASSWORD=$GAMBLEY_MAIL_PASSWORD" >> gambley.env
-echo "PORT=8000" >> gambley.env
+{
+    echo "GAMBLEY_MAIL_USERNAME=$GAMBLEY_MAIL_USERNAME"
+    echo "GAMBLEY_MAIL_PASSWORD=$GAMBLEY_MAIL_PASSWORD"
+    echo "PORT=8000"
+} >> gambley.env
 
 aws s3 cp gambley.env s3://gambley-app-data/configs/gambley.env
 aws s3 cp .docker/stack.yml s3://gambley-app-data/stack/gambley-stack.yml
