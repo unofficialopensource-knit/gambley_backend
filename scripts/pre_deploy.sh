6a3937ab0729e0c61bfab4ee593c39a768f57cca
Mayank Pathak <onlinejudge95@gmail.com>
2021-07-12 00:53:54 +0530

    Adds port in env var file

--- scripts/pre_deploy.sh
+++ scripts/pre_deploy.sh
@@ -6,6 +6,7 @@ sed --in-place "s/BACKEND_IMAGE_TAG/$IMAGE_TAG/g" .docker/stack.yml
 
 echo "GAMBLEY_MAIL_USERNAME=$GAMBLEY_MAIL_USERNAME" >> gambley.env
 echo "GAMBLEY_MAIL_PASSWORD=$GAMBLEY_MAIL_PASSWORD" >> gambley.env
+echo "PORT=8000" >> gambley.env
 
 aws s3 cp gambley.env s3://gambley-app-data/configs/gambley.env
 aws s3 cp .docker/stack.yml s3://gambley-app-data/stack/gambley-stack.yml
